5e219a3a22a9a71e12754bd137818dccfd13fb13
Thakur Nitendra <nitendra.thakur@p34662.intra.rakuten.co.jp>
2017-03-24 18:48:45 +0900

    scripts and util progs

---
+++ Util/src/com/nit/soft/Crypto.java
@@ -0,0 +1,31 @@
+package com.nit.soft;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class Crypto {
+    final static int ITERATIONS = 65536;
+    final static int KEY_LENGTH = 256;
+
+    public static String cypher(String pass, String salt) {
+        byte[] secret = null;
+        try {
+            KeySpec pbeKeySpec = new PBEKeySpec(pass.toCharArray(), salt.getBytes(), ITERATIONS, KEY_LENGTH);
+            SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            secret = secretKeyFactory.generateSecret(pbeKeySpec).getEncoded();
+        } catch(NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+        }
+        System.out.println(secret);
+        Base64.Encoder encoder = Base64.getEncoder();
+        return secret == null ? null : encoder.encodeToString(secret);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(cypher(args[0], args[1]));
+    }
+}
---
+++ scripts/ruby/hello.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby -w
+
+# command to open ruby shell: irb
+# execute this script type: ruby hello.rb
+
+
+puts "Hello, Ruby!";
